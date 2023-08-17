43d2bacefff59d008d6e6b73bfdca434446a003e
Ali Raza <aliraza137725@gmail.com>
2023-08-17 16:48:50 +0500

    practice of import and exprt fo the components

---
+++ script.js
@@ -0,0 +1,27 @@
+// import function
+
+const function1=()=>{
+    return(
+    <> <h3>We export the code from this file and import the code in the script2.js file </h3>
+    </>
+    )
+};
+export default function1;
+
+
+const function2=()=>{
+    return(
+    <> <h3>We export the code from this file and import the code in the script2.js file </h3>
+    </>
+    )
+};
+export default function2;
+
+
+const function3=()=>{
+    return(
+    <> <h3>We export the code from this file and import the code in the script2.js file </h3>
+    </>
+    )
+};
+export default function3;
---
+++ script2.js
@@ -0,0 +1,9 @@
+
+// if we import only 1 component/file from the other file then we use this method, we import single component/file. 
+ 
+import function1 from "./script";
+
+
+//But if we import only more than 1 component/file from the other file then we use this method, we import multiple component/files in single line.
+
+import {function1, function2, function3} from "./script"
